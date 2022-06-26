166f0b5474cc9b2a34cd2b4e4e9d9bad89c6a6e6
RATSENO <onestar1014@gmail.com>
2022-06-26 21:56:21 +0900

    undefined를 렌더링 하지 않기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App(){
-  const name = '뤼액트';
-  return (
-    <div>
-      {name=== '리액트' && <h1>리액트입니다.</h1>}
-    </div>
-  );
+  const name = undefined;
+  return <div>{name || '리액트'}</div>;
 }
 export default App;
