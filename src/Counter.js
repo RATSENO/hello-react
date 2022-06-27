19787a2a8a84fdcb5919d29ef77dc2aec344b6be
RATSENO <onestar1014@gmail.com>
2022-06-27 23:42:01 +0900

    this.setState가 끝난 후 특정 작업 실행하기

--- src/Counter.js
+++ src/Counter.js
@@ -14,17 +14,11 @@ class Counter extends Component {
         <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1,
-              };
-            });
-
-            /*      
-            this.setState(prevState=>{
-                number: prevState + 1
-            }); 
-            */
+            this.setState({
+                number : number +1
+            }, ()=>{
+                console.log('방금 setState가 호출되었습니다.')
+            })
           }}
         >
           +1
