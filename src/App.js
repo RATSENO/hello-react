4febd4c3df00162d787a6d8b8e903562f81d8e56
RATSENO <onestar1014@gmail.com>
2022-06-27 21:46:42 +0900

    컴포넌트를 사용할 때 props 값 지정하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import MyComponent from "./MyComponent";
 import "./App.css";
 
-function App() {
-  return <MyComponent></MyComponent>
+const App = () => {
+  return <MyComponent name="React"></MyComponent>
 }
+
 export default App;
