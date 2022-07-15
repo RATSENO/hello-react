d7e53be428eb628b22bc631eda8916981f22ef67
RATSENO <onestar1014@gmail.com>
2022-07-15 17:16:13 +0900

    라이프 사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
-class App extends Component {
-  render() {
-    return (
-      <IterationSample></IterationSample>
-    )
-  }
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 167777215).toString(16);
 }
 
+class App extends Component {
+  state = {
+    color: "#00000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}></LifeCycleSample>
+      </div>
+    );
+  }
+}
 
 export default App;
